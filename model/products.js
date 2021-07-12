b461cbb36fd477d687982ed139f6d97b327d8f17
TemiRise <temiswift@gmail.com>
2021-07-12 17:30:02 +0100

    week6task

---
+++ model/products.js
@@ -0,0 +1,59 @@
+const products = [
+    {
+        id : 01,
+        name: 'mouse',
+        description: 'large',
+        image: 'black',
+        price: 5000
+    },
+    {
+        id : 02,
+        name: 'phone',
+        description: 'versatile',
+        image: 'black',
+        price: 20000,
+    },
+    {
+        id : 03,
+        name: 'ipad',
+        description: 'sleek',
+        image: 'white',
+        price: 200000,
+    },
+    {
+        id : 04,
+        name: 'backpack',
+        description: 'roomy',
+        image: 'brown',
+        price: 25000,
+    },
+    {
+        id : 05,
+        name: 'joystick',
+        description: 'long',
+        image: 'lightweight',
+        price: 2000,
+    },
+    {
+        id : 06,
+        name: 'console',
+        description: 'versatile',
+        image: 'bold',
+        price: 12000,
+    },
+    {
+        id : 07,
+        name: 'ipad',
+        description: 'sleek',
+        image: 'white',
+        price: 200000,
+    },
+    {
+        id : 08,
+        name: 'phone pouch',
+        description: 'girlie',
+        image: 'pink',
+        price: 10000,
+    },
+]
+module.exports = products;
