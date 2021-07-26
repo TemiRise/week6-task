d405f15a8c5fb16d74a0163948baabd729ada557
TemiRise <temiswift@gmail.com>
2021-07-26 13:23:31 +0100

    updates to week 6

---
+++ models/User.js
@@ -0,0 +1,46 @@
+let user = [
+    {
+        id: 01,
+        email: "temsi@gmail.com",
+        fullName: "Temi Ori",
+        password: "08165423567"
+    },
+
+    {
+        id: 02,
+        email: "tosin@gmail.com",
+        fullName: "John Tosin",
+        password: "09034683699"
+    },
+
+    {
+        id: 03,
+        email: "TolD@gmail.com",
+        fullName: "Tolu Drey",
+        password: "09034623693"
+    },
+
+    {
+        id: 04,
+        email: "xander4@gmail.com",
+        fullName: "Ada Xander",
+        password: "09034683325"
+    },
+
+    {
+        id: 05,
+        email: "weddingworld@gmail.com",
+        fullName: "Ogu Davies",
+        password: "08034683699"
+    },
+
+    {
+        id: 06,
+        email: "tanda@gmail.com",
+        fullName: "Ibrahim Tanda",
+        password: "09087683699"
+    }
+
+];
+
+module.exports = user;
